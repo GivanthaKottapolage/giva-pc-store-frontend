3ed926d33f7f82b1c2106aea26fab87151138235
GivanthaKottapolage <sameeragivantha@gmail.com>
2026-06-28 22:45:20 +0530

    feat(home): redesign homepage layout with dynamic slogans and value propositions

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,30 +4,26 @@ import ProductPage from "./productPage";
 import ProductOverview from "./productOverview";
 import CartPage from "./cart";
 import CheckoutPage from "./checkOut";
+import OrdersPage from "./ordersPage";
+import Home from "./homeContent";
 
-
-export default function HomePage() {
-    return (
+export default function HomePage(){
+    return(
         <div className="w-full h-full overflow-y-scroll max-h-full">
-            <Header />
-            <div className = "w-full min-h-[calc(100%-100px)] ">
-
-                <Routes path="/">
-                    <Route path="/" element={<h1>Home Page</h1>} />
+            <Header/>
+            <div className="w-full min-h-[calc(100%-100px)]">
+                <Routes>
+                    <Route path="/" element={<Home/>} />
                     <Route path="/products" element={<ProductPage />} />
                     <Route path="/about" element={<h1>About Page</h1>} />
-                    <Route path="/contacts" element={<h1>Contact Page</h1>} />
+                    <Route path="/contact" element={<h1>Contact Page</h1>} />
                     <Route path="/overview/:productID" element={<ProductOverview/>} />
                     <Route path="/cart" element={<CartPage />} />
                     <Route path="/checkout" element={<CheckoutPage />} />
-                    <Route path="/*" element={<h1>Page Not Found</h1>} />
-
+                    <Route path="/orders" element={<OrdersPage />} />
+                    <Route path="/*" element={<h1>Page not found</h1>} />
                 </Routes>
-
-                
             </div>
-
-
         </div>
     )
 }
